e8f729b5d60358bde17ed67ed30be0cbaccd63ea
Jake Waffle <2895324+jawaff@users.noreply.github.com>
2024-06-05 18:16:10 -0600

    Added prerequisite dependency

--- install/matrix-install.sh
+++ install/matrix-install.sh
@@ -23,8 +23,9 @@ $STD apt-get install -y \
   python3 \
   python3-dev \
   python3-pip
-$STD apt-get update
-$STD apt-get install ca-certificates
+$STD apt-get install -y \
+  wget \
+  gpg
 UBUNTU_CODENAME=jammy
 $STD wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
 $STD echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/ansible.list
