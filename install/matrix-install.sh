8c21d35d6e448b06944b97b23aac12cc59626ddf
Jake Waffle <2895324+jawaff@users.noreply.github.com>
2024-06-05 18:54:00 -0600

    Final fix to keyring download

--- install/matrix-install.sh
+++ install/matrix-install.sh
@@ -27,9 +27,9 @@ $STD apt-get install -y ca-certificates
 $STD apt-get install -y gpg
 $STD apt-get install -y wget
 UBUNTU_CODENAME=jammy
-$STD wget -qO- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
-$STD echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/ansible.list
-$STD apt update && apt-get install ansible
+$STD wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+$STD echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
+$STD sudo apt update && sudo apt install ansible
 $STD pip install passlib
 $STD apt-get install -y git
 msg_ok "Installed Dependencies"
