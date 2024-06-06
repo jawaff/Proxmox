44ff0fb7c52bd4ef806b386593b84bbe2b6489e5
Jake Waffle <2895324+jawaff@users.noreply.github.com>
2024-06-05 19:00:26 -0600

    New ansible download strategy

--- install/matrix-install.sh
+++ install/matrix-install.sh
@@ -23,13 +23,7 @@ $STD apt-get install -y \
   python3 \
   python3-dev \
   python3-pip
-$STD apt-get install -y ca-certificates
-$STD apt-get install -y gpg
-$STD apt-get install -y wget
-UBUNTU_CODENAME=jammy
-$STD wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
-$STD echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
-$STD sudo apt update && sudo apt install ansible
+$STD apt-get install -y ansible-core 
 $STD pip install passlib
 $STD apt-get install -y git
 msg_ok "Installed Dependencies"
